2830fea75c32f082b5276e77b3b8ac52ce4eb2cb
Bioga Allen <allenojuka10@gmail.com>
2022-07-04 14:36:08 +0300

    Reduced scroll line opacity.

--- components/Scroll/index.tsx
+++ components/Scroll/index.tsx
@@ -3,7 +3,7 @@ import { CgArrowLongDown } from 'react-icons/cg'
 const Scroll = () => {
   return (
     <div className='absolute top-0 left-1/2 -translate-x-1/2 h-full flex flex-col items-center'>
-      <div className='h-full w-[1px] bg-white/10'/>
+      <div className='h-full w-[1px] bg-white/20'/>
       <a href="#footer" className='absolute w-12 h-12 rounded-full border-2 border-white/30
       bottom-[10%] flex items-center justify-center hover:border-white transition duration-200'>
         <CgArrowLongDown />
